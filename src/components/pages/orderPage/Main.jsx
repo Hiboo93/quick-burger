20f574239c099bb4b629cdcb219c8c81657bb826
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2023-08-24 13:39:02 +0200

    🚧 F05: ORDER > MAIN > Add grid layout and Add new files fakeData and Maths

--- src/components/pages/orderPage/Main.jsx
+++ src/components/pages/orderPage/Main.jsx
@@ -4,7 +4,10 @@ import { theme } from '../../../theme/index.js';
 
 function Main() {
   return (
-    <MainStyled className="main"></MainStyled>
+    <MainStyled className="main">
+      <div className="basket">Basket</div>
+      <div className="menu">Menu</div>
+    </MainStyled>
   )
 }
 
@@ -15,6 +18,17 @@ const MainStyled = styled.div`
       border-bottom-left-radius: ${theme.borderRadius.extraRound};
       border-bottom-right-radius: ${theme.borderRadius.extraRound};
       box-shadow: 0px 8px 20px 8px rgba(0,0,0,0.2)inset;
+
+      display: grid;
+      grid-template-columns: 25% 1fr;
+
+      .basket {
+        background: pink;
+      }
+
+      .menu {
+        background: purple;
+      }
 `;
 
 export default Main
