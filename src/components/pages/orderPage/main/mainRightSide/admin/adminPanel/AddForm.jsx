644c6694470ea528d00f7ff88b36938b1a7cebb2
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2023-09-21 19:34:17 +0200

    ♻️ F08: AddForm > Extract ImagePreview

--- src/components/pages/orderPage/main/mainRightSide/admin/adminPanel/AddForm.jsx
+++ src/components/pages/orderPage/main/mainRightSide/admin/adminPanel/AddForm.jsx
@@ -8,6 +8,7 @@ import { MdOutlineEuro } from 'react-icons/md';
 import { theme } from '../../../../../../../theme/index.js';
 import TextInput from '../../../../../../reusable-ui/TextInput.jsx';
 import Button from '../../../../../../reusable-ui/Button.jsx';
+import ImagePreview from './ImagePreview.jsx';
 //import PrimaryButton from '../../../../../../reusable-ui/PrimaryButton.jsx';
 
 
@@ -51,13 +52,7 @@ export default function AddForm() {
 
   return (
     <AddFormStyled action='submit' onSubmit={handleSubmit}>
-      <div className="image-preview">
-        {newProduct.imageSource ? (
-          <img src={newProduct.imageSource} alt={newProduct.title} />
-        ) : (
-          <di className="empty-image">Aucune Image</di>
-        )}
-      </div>
+      <ImagePreview imageSource={newProduct.imageSource} title={newProduct.title}/>
       <div className="input-fields">
       <TextInput
           name="title"
@@ -111,31 +106,7 @@ const AddFormStyled = styled.form`
   grid-row-gap: 8px;
 
 
-  .image-preview {
-    grid-area: 1 / 1 / 4 / 2;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-
-    .empty-image {
-      height: 100%;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border: 1px solid ${theme.colors.greyLight};
-      line-height: 1.5;
-      color: ${theme.colors.greySemiDark};
-      border-radius: ${theme.borderRadius.round};
-    }
-  }
+  
 
   .input-fields {
     //background: blue;
