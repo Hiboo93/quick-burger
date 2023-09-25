52973ea78c10dd12e8f8bafbde5f37836d320deb
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2023-09-25 14:06:42 +0200

    ♻️ F09: AddForm > Add inputTextConfig

--- src/components/pages/orderPage/main/mainRightSide/admin/adminPanel/EditForm.jsx
+++ src/components/pages/orderPage/main/mainRightSide/admin/adminPanel/EditForm.jsx
@@ -1,15 +1,70 @@
 import { useContext } from 'react'
 import HintMessage from './HintMessage.jsx'
 import OrderContext from '../../../../../../../context/OrderContext.jsx'
+import styled from 'styled-components'
+//import Button from '../../../../../../reusable-ui/Button.jsx'
+import ImagePreview from './ImagePreview.jsx'
+import TextInput from '../../../../../../reusable-ui/TextInput.jsx'
+//import { getInputTextsConfig } from '../../../../../../../utils/inputTextConfig.js'
 
 export default function EditForm() {
 
-  const {productSelected} = useContext(OrderContext)
+  //const {productSelected} = useContext(OrderContext)
+
+  //const inputTexts = inputTexts
 
   return (
-    <div>
-      <HintMessage/>
-      <span>{productSelected && productSelected.title}</span>
-    </div>
+    <EditFormStyled >
+      <ImagePreview
+        imageSource={newProduct.imageSource}
+        title={newProduct.title}
+      />
+      <div className="input-fields">
+        {inputTexts.map((input) => (
+          <TextInput
+            key={input.id}
+            name={input.name}
+            value={input.value}
+            placeholder={input.placeholder}
+            Icon={input.Icon}
+            onChange={handleChange}
+            version="minimalist"
+          />
+        ))}
+      </div>
+      <div className="submit"></div>
+    </EditFormStyled>
   )
 }
+
+const EditFormStyled = styled.form`
+  //border: 2px solid black;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr); //grid-template-rows: 1fr 1fr 1fr 1fr;
+  height: 100%;
+  width: 70%;
+  grid-column-gap: 20px;
+  grid-row-gap: 8px;
+
+  .input-fields {
+    //background: blue;
+    grid-area: 1 / 2 / -2 / 3;
+
+    display: grid;
+    grid-row-gap: 8px;
+  }
+
+  .submit {
+    grid-area: 4 / -2 / -1 / -1;
+    display: flex;
+    align-items: center;
+    position: relative;
+    top: 3px;
+
+    .submit-button {
+      //width: 50%;
+      height: 100%;
+    }
+  }
+`;
